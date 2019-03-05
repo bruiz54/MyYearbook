3613f2b0641c2543fc9116f76f1c4bc239291d97
bruiz54 <44578945+bruiz54@users.noreply.github.com>
2019-03-05 11:00:25 -0500

    WebViews 2
    WebView functional and on a separate page
    Button implemented
    Debugging

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myyearbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView wv =  findViewById(R.id.myWebView);
-        wv.loadUrl("https://www.store.steampowered.com");
+
+
+        Button button = findViewById(R.id.myButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
